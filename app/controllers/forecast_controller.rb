ba15821350e9f9859ed3e02056833d1d71fc1514
Shai Coleman <git@shaicoleman.com>
2017-03-19 18:21:55 +0000

    Error handling

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,10 +1,12 @@
 class ForecastController < ApplicationController
   def index
     @city = params[:city]
+    @input_error = @city && @city.blank?
     return unless @city.present?
     owm = OpenWeatherMapClient.new
     @current = owm.current_conditions(city: @city)
     @forecast = owm.forecast(city: @city, days: 16)
   rescue OpenWeatherMapClient::Error => e
+    @error = e.message
   end
 end
